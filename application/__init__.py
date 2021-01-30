7eeea17060b99660c4426ac15cbb5e6a9d018d96
Breno Cocheto <48560880+bcocheto@users.noreply.github.com>
2021-01-30 15:22:55 -0300

    feat: Api
    Criada conexão com o banco de dados

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 import os
+
 
 app = Flask(
     __name__,
     static_folder=os.path.abspath("application/view/static"),
     template_folder=os.path.abspath("application/view/templates"),
 )
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://postgres:root@127.0.0.1:5432/db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+from application.model.api import Client
+
+db.create_all()
+db.session.commit()
 
 from application.controller import home_controller
---
+++ application/model/api.py
@@ -0,0 +1,17 @@
+from application import db
+
+
+class Client(db.Model):
+    __tablename__ = "cliente"
+    codigo = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(64), unique=True)
+    razao_social = db.Column(db.String(64), unique=True)
+    cnpj = db.Column(db.String(64), unique=True)
+    data_inclusao = db.Column(db.DateTime)
+
+    def __init__(self, codigo, nome, razao_social, cnpj, data_inclusao):
+        self.codigo = codigo
+        self.nome = nome
+        self.razao_social = razao_social
+        self.cnpj = cnpj
+        self.data_inclusao = data_inclusao
